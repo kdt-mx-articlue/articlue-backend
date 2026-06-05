d5b6937bc912af1b6a2777e25572b532500d99c0
JMS <lacam751@gmail.com>
2026-06-05 13:18:37 +0900

    테스트 완료

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,24 +1,15 @@
 const express = require("express");
-
 const router = express.Router();
-
-const authController =
-    require("../controllers/auth.controller");
+const authController = require("../controllers/auth.controller");
 
 /**
  * 회원가입
  */
-router.post(
-    "/signup",
-    authController.signup
-);
+router.post("/signup", authController.signup);
 
 /**
  * 로그인
  */
-router.post(
-    "/login",
-    authController.login
-);
+router.post("/login", authController.login);
 
 module.exports = router;
